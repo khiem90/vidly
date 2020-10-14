14f3b004476c5084cca230d4ddf846161e9b5277
unknown <khiemduy90@gmail.com>
2020-10-14 15:40:02 -0500

    Movies component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Movies from './components/movies';
 
 function App() {
   return (
    <main className="container">
-
+     <Movies />
    </main>
   );
 }
